dbb687721129410a3e84e4a9d74ac205ed796036
annisadiann <annisadian0412@gmail.com>
2025-10-30 15:49:25 +0700

    Tambah garis pemisah sebelum tombol submit

--- app/src/main/java/com/example/praktikum4/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4/ui/theme/TeksEdit.kt
@@ -81,3 +81,10 @@ fun FormDataDiri(modifier: Modifier
             onValueChange = {
                 textAlamat = it
             })
+        HorizontalDivider(
+            modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium), top = dimensionResource(
+                id = R.dimen.padding_medium
+            )),
+            thickness = dimensionResource(R.dimen.divider_tipis),
+            color = Color.DarkGray
+        )
