0d2bcfbfeb4b572552b8e9f1eb7025ebe58ceb96
annisadiann <annisadian0412@gmail.com>
2025-10-30 15:38:25 +0700

    Import UI dan resource tambahan

--- app/src/main/java/com/example/praktikum4/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4/ui/theme/TeksEdit.kt
@@ -27,3 +27,5 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
