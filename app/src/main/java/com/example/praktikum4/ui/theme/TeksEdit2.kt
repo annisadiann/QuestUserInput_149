b713b2c6155e43dba9d9dbecba4a894212b36837
annisadiann <annisadian0412@gmail.com>
2025-11-01 10:38:40 +0700

    Tambah input field Alamat

--- app/src/main/java/com/example/praktikum4/ui/theme/TeksEdit2.kt
+++ app/src/main/java/com/example/praktikum4/ui/theme/TeksEdit2.kt
@@ -175,3 +175,29 @@ fun FormDataDiri1(modifier: Modifier = Modifier) {
                 }
             }
 
+            // Alamat
+            Text(
+                text = "ALAMAT",
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Medium,
+                color = Color.Gray,
+                modifier = Modifier.padding(top = 20.dp, bottom = 8.dp)
+            )
+            OutlinedTextField(
+                value = textAlamat,
+                singleLine = true,
+                placeholder = { Text(text = "Alamat", color = Color.Gray) },
+                shape = RoundedCornerShape(8.dp),
+                onValueChange = {
+                    textAlamat = it
+                },
+                modifier = Modifier.fillMaxWidth(),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedContainerColor = Color.White,
+                    unfocusedContainerColor = Color.White,
+                    focusedBorderColor = Color.LightGray,
+                    unfocusedBorderColor = Color.LightGray
+                )
+            )
+        }
+
